b80e442c1a2e73bd9d42a85b1afba99b95e94bbe
michael carlos <michael82carlos@gmail.com>
2014-11-19 11:08:07 +0800

    UI01

--- gen/com/vip/syreadhd/R.java
+++ gen/com/vip/syreadhd/R.java
@@ -39,7 +39,9 @@ public final class R {
         public static final int star=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int img_rainbow=0x7f080001;
+        public static final int img_star=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
